44c8c596de11226174b7dbed4a241a1f26515a77
AyonJD <ayonjodder177@gmail.com>
2023-08-06 14:12:16 +0600

    React router setup done

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import NotFound from '../pages/NotFound'
+
+const routes = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/*', element: <NotFound /> },
+])
+
+export default routes
